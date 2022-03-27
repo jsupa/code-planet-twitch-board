1e1c94deaf9c80417fa673780aaf6ae44c5fd45f
Jakub Supa <jakub.supa@icloud.com>
2022-03-27 15:25:10 +0200

    👨💻 create data & helpers

---
+++ server/lib/helpers.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto')
+const fs = require('fs')
+
+const config = require('./config')
+
+const helpers = {}
+
+helpers.getStaticAsset = (fileName, callback) => {
+  fileName = typeof fileName === 'string' && fileName.length > 0 ? fileName : false
+  if (fileName) {
+    const publicDir = './client/public/'
+    fs.readFile(publicDir + fileName, (err, data) => {
+      if (!err && data) {
+        callback(false, data)
+      } else {
+        callback('No file could be found')
+      }
+    })
+  } else {
+    callback('A valid file name was not specified')
+  }
+}
+
+helpers.hash = str => {
+  if (typeof str === 'string' && str.length > 0) {
+    const hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex')
+    console.log(hash)
+    return hash
+  } else {
+    return false
+  }
+}
+
+module.exports = helpers
