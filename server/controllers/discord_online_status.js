57ae7fda1f5fca2c431136d8c8b759aa246e821e
Jakub Supa <jakub.supa@icloud.com>
2022-04-05 23:26:55 +0200

    Form & views update

--- server/controllers/discord_online_status.js
+++ server/controllers/discord_online_status.js
@@ -4,23 +4,43 @@ module.exports.index = async (data, req, res) => {
 }
 
 module.exports.settings = async (data, req, res) => {
-  data.titleoverwrite = '🪛 Discord Online Status Settings'
+  data.titleoverwrite = '🪛 Discord Embed Settings'
   const FORM_INPUTS = {
-    message_content: { type: 'text', placeholder: 'Message Content', extraClass: 'm_input' },
+    message_content: {
+      type: 'text',
+      placeholder: 'Message Content',
+      extraClass: 'm_input',
+      required: true,
+      default: 'Hi @everyone'
+    },
     message_color: { type: 'color', placeholder: 'Message Color', default: '#F29595' },
-    author_name: { type: 'text', placeholder: 'Author Name', default: 'Discord Online Status' },
-    author_icon_url: { type: 'link', placeholder: 'Author Icon URL' },
-    title: { type: 'text', placeholder: 'Title' },
-    description: { type: 'text', placeholder: 'Description' },
-    link: { type: 'link', placeholder: 'Link', default: 'https://discord.gg/' },
-    thumbnail_url: { type: 'link', placeholder: 'Thumbnail URL' },
+    author_name: {
+      type: 'text',
+      placeholder: 'Author Name',
+      required: true,
+      default: '{user_name} is now live on Twitch!'
+    },
+    author_icon_url: { type: 'link', placeholder: 'Author Icon URL', required: true, default: '{user_avatar_url}' },
+    title: { type: 'text', placeholder: 'Title', required: true, default: '{title}' },
+    description: { type: 'text', placeholder: 'Description', required: true, default: 'Playing {game}' },
+    link: { type: 'link', placeholder: 'Link', default: 'https://twitch.tv/{user_name}' },
+    thumbnail_url: {
+      type: 'link',
+      placeholder: 'Thumbnail URL',
+      default: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_{user_name}-1920x1080.jpg'
+    },
     footer_text: {
       type: 'text',
       placeholder: 'Footer Text',
       disabled: true,
-      default: 'Made with ❤️ by www.code-planet.eu'
+      default: 'Made with ❤️ by www.code-planet.eu - {time}'
     },
-    footer_icon_url: { type: 'link', placeholder: 'Footer Icon URL', disabled: true, default: 'https://discord.gg/' }
+    footer_icon_url: {
+      type: 'link',
+      placeholder: 'Footer Icon URL',
+      disabled: true,
+      default: 'https://code-planet.eu/images/Logo.png'
+    }
   }
   const FORM_ROWS = [
     { message_content: FORM_INPUTS.message_content },
@@ -30,5 +50,15 @@ module.exports.settings = async (data, req, res) => {
     { thumbnail_url: FORM_INPUTS.thumbnail_url },
     { footer_text: FORM_INPUTS.footer_text, footer_icon_url: FORM_INPUTS.footer_icon_url }
   ]
-  res.render('form', { data, FORM_ROWS })
+  const LEGEND_ROWS = [
+    { '{user_name}': 'Twitch Username.' },
+    { '{user_avatar_url}': 'Twitch Avatar URL.' },
+    { '{title}': 'Twitch Title.' },
+    { '{game}': 'Twitch Game.' },
+    { '{time}': 'Current Time.' }
+  ]
+  if (req.method === 'GET') res.render('settings', { data, FORM_ROWS, LEGEND_ROWS })
+  else if (req.method === 'POST') {
+    res.render('settings', { data, FORM_ROWS, LEGEND_ROWS })
+  }
 }
